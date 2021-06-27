534c6f61ee5edd9bd7b73fa03126843fd9fee30e
Shehan Abeyrathne <shehan.abeyrathne96@gmail.com>
2021-06-27 10:49:38 +0530

    Update Hello.js

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function Hello() {
-    return <h1>Hello Shehan</h1>;
+    return (
+        <>
+            <h1>Hello Shehan</h1>
+            <p>This is awesome</p>
+        </>
+    )
 }
 
 export default Hello;
