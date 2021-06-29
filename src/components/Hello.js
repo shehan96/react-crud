20f8fb015de7fe38566277b7bab58ebfdb86737c
Shehan Abeyrathne <shehan.abeyrathne96@gmail.com>
2021-06-29 22:17:48 +0530

    Update Hello.js

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 function Hello() {
+<<<<<<< Updated upstream
     return <h1>Hello Shehan</h1>;
+=======
+    return (
+        <>
+            <h1>Hello Shehan</h1>
+            <p>This is awesome</p>
+            <h3>Another tag</h3>
+        </>
+    )
+>>>>>>> Stashed changes
 }
 
 export default Hello;
